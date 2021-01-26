14a5e53067c6a47d0f26b9dc73f5c32303bbcd83
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2021-01-25 23:22:16 -0800

    End of episode 2, OLED setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
+#define OLED_CLOCK 15     // Pins for OLED display
+#define OLED_DATA 4
+#define OLED_RESET 16
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_oled(U8G2_R2, OLED_CLOCK, OLED_DATA, OLED_RESET);
+int g_linehight = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN,OUTPUT);
+
+  g_oled.begin();
+  g_oled.clear(); //sets curser at 0,0. Text draws from the bottom up so you will see nothing.
+  g_oled.setFont(u8g2_font_profont15_tf);
+  g_linehight = g_oled.getFontAscent() - g_oled.getFontDescent(); // Decent is a negative number so we add it to the total
+  g_oled.setCursor(0,g_linehight);                                // Place cursor at bottom of first line of text
+  g_oled.print("Dave's Garage");
+  g_oled.printf("Line height: %d", g_linehight);
+  g_oled.sendBuffer();                                            // Print it out to the OLED
 }
 
 void loop() {
