093bc8bb1d484692530fd3a7f1c38cde87c89dd7
dmsloan from Dell Latitude <dsloan@ibe-inc.com>
2021-01-28 00:48:20 -0800

    Episode 4 finished

--- src/main.cpp
+++ src/main.cpp
@@ -30,26 +30,27 @@
   #define OLED_CLOCK 15              // Pins for OLED display
   #define OLED_DATA 4
   #define OLED_RESET 16
-  #define LED_PIN 23 //Dave recomends pin 5 but it is being used by LoRa on my board
+  #define LED_PIN 23 //Output pin for the WS2812B led strip. Dave recomends pin 5 but it is being used by LoRa on my board
 #elif defined(ARDUINO_LOLIN32)
   #define OLED_CLOCK 4              // Pins for OLED display
   #define OLED_DATA 5
   #define OLED_RESET 16
-  #define LED_PIN 5
+  #define LED_PIN 5 //Output pin for the WS2812B led strip.
 #else
   #define OLED_CLOCK 4              // Pins for OLED display
   #define OLED_DATA 5
   #define OLED_RESET 16
-  #define LED_PIN 5
+  #define LED_PIN 5 //Output pin for the WS2812B led strip.
 #endif
 
 #define NUM_LEDS 300             // FastLED definitions
 CRGB g_LEDs[NUM_LEDS] = {0};     //Frame buffer for FastLED
 int g_Brightness = 16;           // 0-255 LED brightness scale
+int g_MaxPowerInMilliWatts = 900; // Max power for the led strip
 
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_oled(U8G2_R2, OLED_CLOCK, OLED_DATA, OLED_RESET); // uses Software I2C and results in a framerate of 5 FPS
 
 //clock and data got swapped around to use hardware I2C instead of software
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C g_oled(U8G2_R2, OLED_CLOCK, OLED_DATA, OLED_RESET); // uses Software I2C and results in a framerate of 5 FPS
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C g_oled(U8G2_R2, OLED_RESET, OLED_CLOCK, OLED_DATA); // uses Hardware I2C and results in a framerate of 26 FPS 
 int g_linehight = 0;
 
@@ -65,15 +66,12 @@ void setup() {
   g_oled.begin();
   g_oled.clear(); //sets curser at 0,0. Text draws from the bottom up so you will see nothing.
   g_oled.setFont(u8g2_font_profont15_tf);
- 
   g_linehight = g_oled.getFontAscent() - g_oled.getFontDescent(); // Decent is a negative number so we add it to the total
  
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(g_LEDs, NUM_LEDS);               // Add our LED strip to the FastLED library
   FastLED.setBrightness(g_Brightness);
-  FastLED.setMaxPowerInMilliWatts(900);
-
+  FastLED.setMaxPowerInMilliWatts(g_MaxPowerInMilliWatts);
 }
-
 
 // FramePerSecond
 //
@@ -124,7 +122,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   static bool bLED = LOW;
-  int fps = 0;
+  double fps = 0;
+
+  uint8_t initialHue = 0;
+  const uint8_t delaHue = 16;
+  const uint8_t hueDensity = 4;
 
   for (;;)                                  // This is a forever loop
     {
@@ -132,7 +134,24 @@ void loop() {
       digitalWrite(LED_BUILTIN,bLED);
 
       double dStart = millis() / 1000.0;    // Record the start time
-      DrawLinesAngGrapicaFrame(fps);
+
+      // Handle OLED drawing
+//      DrawLinesAngGrapicaFrame(fps);
+      g_oled.clearBuffer();
+      g_oled.setCursor(9, g_linehight);
+      g_oled.printf("FPS: %.1lf", fps);
+      g_oled.sendBuffer();
+
+      // Handle LEDs
+      // fill all leds with a loop
+      // for (int i = 0; i < NUM_LEDS; i++)
+      //   g_LEDs[i] = CRGB::White;
+      // FastLED.show();
+
+      // fill_solid(g_LEDs, NUM_LEDS,CRGB::Green); // fills with a function form FastLed instead of using a loop
+      fill_rainbow(g_LEDs, NUM_LEDS,initialHue += hueDensity, delaHue); // fills with a function form FastLed instead of using a loop
+      FastLED.show();
+
       double dEnd = millis() / 1000.0;      // Record the finish time
       fps = FramesPerSecond(dEnd - dStart); // Calculate the FPS rate
     }
